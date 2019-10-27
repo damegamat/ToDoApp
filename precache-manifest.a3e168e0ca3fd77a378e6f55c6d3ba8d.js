16604c99a2905e4952a57cc486de05074287d797
Darek <dar.lewandowski93@gmail.com>
2019-10-28 00:57:53 +0100

    Updates

---
+++ precache-manifest.a3e168e0ca3fd77a378e6f55c6d3ba8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d2692040ad47be3f2b6",
+    "url": "/static/js/main.3e2bc5c4.chunk.js"
+  },
+  {
+    "revision": "c4f9d12e475d3c2c3191",
+    "url": "/static/js/2.54a8cf50.chunk.js"
+  },
+  {
+    "revision": "5d2692040ad47be3f2b6",
+    "url": "/static/css/main.beaefcd1.chunk.css"
+  },
+  {
+    "revision": "2d6f21d46209c57130b0265438eb3b8f",
+    "url": "/index.html"
+  }
+];
