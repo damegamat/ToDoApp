c34ce91136e5126afe2206ffc60f79edb0920b2f
Darek <dar.lewandowski93@gmail.com>
2019-10-27 23:59:39 +0100

    update styles

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,18 @@
 import React from "react";
+import "./Task.css";
 
 const Task = props => {
   const { id, text, date, active, important, finishDate } = props.task;
 
   if (active) {
     return (
-      <div>
+      <div className="activeTask">
         <p>
           <strong className={important ? "priority" : ""}>{text}</strong>
           to <span>{date}</span>
-          <button onClick={() => props.status(id)}>Done</button>
+          <button onClick={() => props.status(id)}>
+            {"Done".toUpperCase()}
+          </button>
           <button onClick={() => props.delete(id)}>X</button>
         </p>
       </div>
@@ -17,11 +20,11 @@ const Task = props => {
   } else {
     const finishTime = new Date(finishDate).toLocaleString();
     return (
-      <div>
+      <div className="deactiveTask">
         <p>
           <strong>{text}</strong>
           <em> to {date}</em>
-          <span>Completed {finishTime}</span>
+          <span> Completed: {finishTime}</span>
           <button onClick={() => props.delete(id)}>X</button>
         </p>
       </div>
